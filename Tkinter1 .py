923efab5ea71301dc1aa57ada093bb18066bebdd
Tareq Assi <42150220+ZF7724@users.noreply.github.com>
2020-06-06 08:28:50 -0400

    Add files via upload

---
+++ Tkinter1 .py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+window=Tk()
+
+b1=Button(window,text="Press Me!")
+b1.grid(row=0,column=0)
+
+e1=Entry(window)
+e1.grid(row=0,column=1)
+
+t1=Text(window,height=2,width=25)
+t1.grid(row=0,column=2)
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
